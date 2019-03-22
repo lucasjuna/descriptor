5d40f9b0e07a80fe3d883679976281b69ee940c4
Lucas San Juan <lucasjuna70@gmail.com>
2019-03-22 20:14:28 +0100

    Client build

--- Descriptor.API/wwwroot/service-worker.js
+++ Descriptor.API/wwwroot/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.040b542f98722ac6221f1f89c3ffddc2.js"
+  "/precache-manifest.8baebe1958e5c50354ab2fe83f692409.js"
 );
 
 workbox.clientsClaim();

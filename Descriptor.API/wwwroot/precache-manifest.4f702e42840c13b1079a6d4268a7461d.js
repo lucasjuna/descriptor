180fd88e07016104c6a78859f3e77dd380a695a8
Lucas San Juan <lucasjuna70@gmail.com>
2019-03-29 08:27:12 +0100

    Client build

---
+++ Descriptor.API/wwwroot/precache-manifest.4f702e42840c13b1079a6d4268a7461d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dd73b457569158709ba3",
+    "url": "/static/js/main.ece8c6a4.chunk.js"
+  },
+  {
+    "revision": "26c59a88290c744f0a18",
+    "url": "/static/js/2.8e5822a8.chunk.js"
+  },
+  {
+    "revision": "dd73b457569158709ba3",
+    "url": "/static/css/main.16390926.chunk.css"
+  },
+  {
+    "revision": "26c59a88290c744f0a18",
+    "url": "/static/css/2.7998abb8.chunk.css"
+  },
+  {
+    "revision": "5030998311011b258427f25bc6e4321d",
+    "url": "/index.html"
+  }
+];
--- Descriptor.API/wwwroot/service-worker.js
+++ Descriptor.API/wwwroot/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5a43d96ccc62c5cd935c1654a724d3a0.js"
+  "/precache-manifest.4f702e42840c13b1079a6d4268a7461d.js"
 );
 
 workbox.clientsClaim();
